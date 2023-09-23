0bd2b4e50972af40930bf7a5abb958708f3bea0b
Chayanon Pissanuwattanasak <chayanoon747@gmail.com>
2023-09-23 21:31:55 +0700

    demo

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { StackNav } from './navigators/StackNav';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <NavigationContainer>
+        <StackNav/>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
