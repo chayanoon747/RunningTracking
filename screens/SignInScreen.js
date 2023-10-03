6c06c370e3686065f1891e1e6cee6c09cb53b9b8
Chayanon Pissanuwattanasak <chayanoon747@gmail.com>
2023-10-03 23:34:55 +0700

    UI SignIn

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native"
 import { TextInput } from "react-native-paper";
 import { useFonts, Roboto_100Thin, Roboto_500Medium, Roboto_700Bold, Roboto_900Black} from '@expo-google-fonts/roboto';
 import { AntDesign, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
@@ -9,6 +9,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 export const SignInScreen = ({navigation})=>{
     const [isEmailFocused, setIsEmailFocused] = useState(false);
     const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+    const { width, height } = Dimensions.get('window');
 
     const [fontsLoaded] = useFonts({
         Roboto_100Thin,
@@ -38,17 +39,19 @@ export const SignInScreen = ({navigation})=>{
 
     return(
         <SafeAreaView style={{flex:1}}>
-            <View style={{flex:1, borderWidth:2, borderColor:'red'}}>
-
+            <View style={{flex:1}}>
+                <View style={{backgroundColor: 'black', width: width, height: 250, position: 'absolute', left: 0, top: '-90%', 
+                                borderBottomLeftRadius: 200, borderBottomRightRadius: 200 }}>
+                </View>
             </View>
-            <View style={{flex:2, borderWidth:2, borderColor:'green', margin:20}}>
+            <View style={{flex:2, margin:20}}>
 
-                <View style={{flex:1.5, borderWidth:2, borderColor:'pink'}}>
+                <View style={{flex:1.5}}>
                     <Text style={{fontFamily: 'Roboto_900Black', fontSize:40, fontWeight:'bold'}}>Login</Text>
                     <Text style={{marginTop:10,fontFamily: 'Roboto_500Medium', fontSize:18, color:'gray', fontWeight:'bold'}}>Please sign in to continue.</Text>
                 </View>
 
-                <View style={{flex:2, borderWidth:2, borderColor:'purple'}}>
+                <View style={{flex:2, borderWidth:3, borderColor:'black'}}>
                     <View style={{flex:1, flexDirection:'row', alignItems:'center', marginRight:20}}>
                         <MaterialCommunityIcons name={isEmailFocused ? 'email-open-outline' : 'email-outline'} size={24} color="black" style={{paddingHorizontal:10}}/>
                         <TextInput style={styles.textinput} 
@@ -74,17 +77,17 @@ export const SignInScreen = ({navigation})=>{
                     
                 </View>
 
-                <View style={{flex:1, flexDirection:'row', borderWidth:2, borderColor:'blue'}}>
-                    <View style={{flex:2, borderWidth:2, borderColor:'pink'}}></View>
-                    <View style={{flex:1.4, borderWidth:2, borderColor:'cyan'}}>
-                        <TouchableOpacity style={{flex:1, marginVertical:'10%'}}
+                <View style={{flex:1, flexDirection:'row'}}>
+                    <View style={{flex:2}}></View>
+                    <View style={{flex:1.4}}>
+                        <TouchableOpacity style={{flex:1, marginVertical:'8%'}}
                             onPress={()=>{
                                 navigation.navigate('BottomTabNav')
                             }}
                         >
                             <LinearGradient 
-                                colors={['#fd5c63','#DE3163', '#E31837']} 
-                                style={{flex:1,flexDirection:'row', borderRadius:50, justifyContent:'center', alignItems:'center'}}
+                                colors={['#000000','#000000']} 
+                                style={{flex:1,flexDirection:'row', borderRadius:20, justifyContent:'center', alignItems:'center'}}
                                 start={{x: 0, y: 0}} end={{x: 1, y: 0}}
                             >
                                 <Text style={{fontFamily: 'Roboto_700Bold', fontSize:20, color:'white', marginRight:'10%'}}>LOGIN</Text>
@@ -94,14 +97,18 @@ export const SignInScreen = ({navigation})=>{
                     </View>
                 </View>
             </View>
-            <View style={{flex:1, flexDirection:'row',borderWidth:2, borderColor:'blue', justifyContent:'center', alignItems:'flex-end'}}>
+
+            <View style={{flex:1, flexDirection:'row', justifyContent:'center', alignItems:'flex-end'}}>
+                <View style={{backgroundColor: 'black', width: width, height: 250, position: 'absolute', left: 0, top: '55%', 
+                            borderTopLeftRadius: 200, borderTopRightRadius: 200 }}>
+                </View>
                 <Text style={{fontFamily: 'Roboto_700Bold', color:'gray'}}>Don't have an account?</Text>
                 <TouchableOpacity
                     onPress={()=>{
                         navigation.navigate('SignUpScreen')
                     }}
                 >
-                    <Text style={{fontFamily: 'Roboto_900Black', color:'orange'}}> Sign Up</Text>
+                    <Text style={{fontFamily: 'Roboto_900Black', color:'#FFFFFF'}}> Sign Up</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
